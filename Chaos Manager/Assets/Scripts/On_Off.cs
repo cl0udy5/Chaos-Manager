c14f3d0e80a9577e9a09fc0f3423309aef9f1ad4
cl0udy5 <ivanzzzorg@gmail.com>
2022-07-16 00:47:07 +0300

    some bullshit

--- Chaos Manager/Assets/Scripts/On_Off.cs
+++ Chaos Manager/Assets/Scripts/On_Off.cs
@@ -9,13 +9,15 @@ public class On_Off : MonoBehaviour
     [SerializeField] Sprite turnedOn;
     [SerializeField] Sprite turnedOff;
     [SerializeField] GameObject NumberOfCases;
+    [SerializeField] GameObject glare;
     bool isTurnedOn;
     
     void Start()
     {
         NumberOfCases.SetActive(false);
+        glare.SetActive(false);
     }
-    
+
     public void Pressed()
     {
         if(isTurnedOn)
@@ -23,12 +25,14 @@ public class On_Off : MonoBehaviour
             screen.sprite = turnedOff;
             isTurnedOn = false;
             NumberOfCases.SetActive(false);
+            glare.SetActive(false);
         }
         else
         {
             screen.sprite = turnedOn;
             isTurnedOn = true;
             NumberOfCases.SetActive(true);
+            glare.SetActive(true);
         }
     }
 }
